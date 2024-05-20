8f4492b2e4c9dd2edaa7340d7a30c83bcaf427e8
Maximilian <maximilian.dybvik@gmail.com>
2024-05-20 21:39:56 +0200

    added styling for tablet-up and different form states

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const formButton = document.getElementById('submit');
+const submitButton = document.getElementById('submit');
 const dismissButton = document.getElementById('dismiss');
+const emailConfirmation = document.getElementById('email-confirmation');
+const errorMessage = document.getElementById('error-message');
+const emailInput = document.getElementById('email');
 const card = document.getElementsByClassName('card');
 const success = document.getElementsByClassName('success');
 
 function validate(e) {
   e.preventDefault();
+  emailInput.classList.toggle('error');
+  errorMessage.classList.toggle('hidden');
+
   console.log('Form NOT validated!');
 }
 
@@ -14,6 +20,7 @@ function toggleState(e) {
   success[0].classList.toggle('visible');
 }
 
-formButton.addEventListener('click', validate);
-formButton.addEventListener('click', toggleState);
+// formButton.addEventListener('click', validate);
+submitButton.addEventListener('click', toggleState);
 dismissButton.addEventListener('click', toggleState);
+dismissButton.addEventListener('click', validate);
